1dc19485c202439a61d96e7250dd1b18eda4dfd3
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2025-11-10 12:54:07 +0530

    chnages committed

--- config/DB.js
+++ config/DB.js
@@ -2,14 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Mongo Atlas DB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
-    process.exit(1);
   }
 };
 
